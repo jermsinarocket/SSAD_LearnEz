52aeed4979f3b90ddb293aec2ef801c35f9fd6d0
Tan Wei Xuan <jermsinarocket@gmail.com>
2019-11-11 22:24:18 +0800

    Final

--- LearnEz/entities/Game/Map.gd
+++ LearnEz/entities/Game/Map.gd
@@ -105,7 +105,7 @@ func generate_enemies():
 		for y in height:
 			nodelist.append(Vector2(x,y))
 	nodelist.pop_front()
-	for x in range(6):
+	for x in range(8):
 		nodelist = shuffleList(nodelist)
 		var pos = nodelist.pop_front()
 		enemyloc.append(pos)
